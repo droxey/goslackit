08f4c7374986f6d3167b203f05c06c51bb7054e6
Dani Roxberry <dani@bitoriented.com>
2019-02-12 11:19:17 -0800

    [fix] add +heroku in go.mod, also add more comments.

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@
 module github.com/droxey/gopherology
 
 require (
-	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/droxey/goslackit v0.0.0-20190212191025-3f9dc7e7815b
 	github.com/joho/godotenv v1.3.0
 	github.com/nlopes/slack v0.5.0
-	github.com/pkg/errors v0.8.1 // indirect
 )
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 )
 
+// main is our entrypoint, where the application initializes the Slackbot.
+// DO NOT EDIT THIS FUNCTION. This is a fully complete implementation.
 func main() {
 	botToken := os.Getenv("BOT_OAUTH_ACCESS_TOKEN")
 	slackClient := slack.CreateSlackClient(botToken)
