7106beca59da5209e2eee77bf08c9450d72d20f9
Dani Roxberry <dani@makeschool.com>
2019-02-13 12:49:24 -0800

    [fix] add @lowewenzel's enhanced main.go

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ import (
 // main is our entrypoint, where the application initializes the Slackbot.
 // DO NOT EDIT THIS FUNCTION. This is a fully complete implementation.
 func main() {
+	port := ":" + os.Getenv("PORT")
+	go http.ListenAndServe(port, nil)
+	slackIt()
+}
+
+func slackIt() {
 	botToken := os.Getenv("BOT_OAUTH_ACCESS_TOKEN")
 	slackClient := slack.CreateSlackClient(botToken)
 	slack.RespondToEvents(slackClient)
